9eba615e565bc964452d0045ad25c5918c536589
Mujahid Hemani <9062439+mujahidhemani@users.noreply.github.com>
2019-11-08 05:14:12 +0000

    create separate backend and frontend subnets in the vpc, create NAT gateways for backend subnets

--- main.tf
+++ main.tf
@@ -12,6 +12,14 @@ resource "aws_subnet" "backend-subnet" {
   count                   = 3
   vpc_id                  = "${aws_vpc.vpc.id}"
   cidr_block              = "${cidrsubnet(var.cidrblock, 3, count.index)}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${element(data.aws_availability_zones.available.names, count.index)}"
+}
+
+resource "aws_subnet" "frontend-subnet" {
+  count = 3
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "${cidrsubnet(var.cidrblock, 3, count.index+4)}"
   map_public_ip_on_launch = true
   availability_zone       = "${element(data.aws_availability_zones.available.names, count.index)}"
 }
@@ -20,23 +28,59 @@ resource "aws_internet_gateway" "internet-gw" {
   vpc_id = "${aws_vpc.vpc.id}"
 }
 
-resource "aws_route_table" "route-table" {
+resource "aws_route_table" "frontend-route-table" {
   vpc_id = "${aws_vpc.vpc.id}"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_route_table" "backend-route-table" {
+  count = 3
+  vpc_id = "${aws_vpc.vpc.id}"
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_route" "default-route" {
-  route_table_id         = "${aws_route_table.route-table.id}"
+resource "aws_route" "backend-default-route" {
+  count = 3
+  route_table_id         = "${element(aws_route_table.backend-route-table.*.id, count.index)}"
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.internet-gw.id}"
+  nat_gateway_id             = "${element(aws_nat_gateway.nat-gw.*.id, count.index)}"
 }
 
-resource "aws_route_table_association" "route-table-association" {
+resource "aws_route" "frontend-default-route" {
+  route_table_id = "${aws_route_table.frontend-route-table.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.internet-gw.id}"
+}
+
+
+resource "aws_route_table_association" "backend-route-table-association" {
   count          = 3
   subnet_id      = "${element(aws_subnet.backend-subnet.*.id, count.index)}"
-  route_table_id = "${aws_route_table.route-table.id}"
+  route_table_id = "${element(aws_route_table.backend-route-table.*.id, count.index)}"
+}
+
+resource "aws_route_table_association" "frontend-route-table-association" {
+  count          = 3
+  subnet_id      = "${element(aws_subnet.frontend-subnet.*.id, count.index)}"
+  route_table_id = "${aws_route_table.frontend-route-table.id}"
 }
 
 data "aws_subnet_ids" "vpc-subnet-ids" {
   vpc_id     = "${aws_vpc.vpc.id}"
   depends_on = ["aws_subnet.backend-subnet"]
 }
+
+resource "aws_nat_gateway" "nat-gw" {
+  count = 3
+  allocation_id = "${element(aws_eip.nat-gw-eip.*.id, count.index)}"
+  subnet_id     = "${element(aws_subnet.frontend-subnet.*.id, count.index)}"
+  depends_on = ["aws_internet_gateway.internet-gw"]
+}
+
+resource "aws_eip" "nat-gw-eip" {
+  count = 3
+}
+
