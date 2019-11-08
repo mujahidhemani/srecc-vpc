0e73bb0686125d282917c48f606fbf6110fd7d90
Mujahid Hemani <9062439+mujahidhemani@users.noreply.github.com>
2019-11-08 05:12:37 +0000

    add separate outputs for backend and frontend subnet ids

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,12 @@ output "all_subnet_ids" {
   value = "${data.aws_subnet_ids.vpc-subnet-ids.ids}"
 }
 
+output "backend_subnet_ids" {
+  value = aws_subnet.backend-subnet.*.id
+}
+
+output "frontend_subnet_ids" {
+  value = aws_subnet.frontend-subnet.*.id
+}
+
+
